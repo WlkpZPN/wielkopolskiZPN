b7634abe97b1b6788833545d6f2aa73cb197f873
Aleksander <hondakkia@gmail.com>
2021-02-12 00:38:02 +0000

    fixed hoc imports

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,37 +1,29 @@
-import { useState,createContext } from 'react';
-import AdminLayout from '../../components/organisms/admin_layout';
-import {protectedAdminRoute} from './protectedAdmin';
-import cookie from 'cookie';
-import nookies from 'nookies';
+import { useState, createContext } from "react";
+import AdminLayout from "../../components/organisms/admin_layout";
+import { protectedAdminRoute } from "../../middleware/protectedAdmin";
+import cookie from "cookie";
+import nookies from "nookies";
 
 export const UserContext = createContext(null);
 
 const MainPage = ({ userData }) => {
-  const [currentView, setCurrentView] = useState('wnioski');
+  const [currentView, setCurrentView] = useState("wnioski");
   console.log(userData);
   return (
-    <UserContext.Provider value={{ userData,
-    setView:setCurrentView,}} >
-
-    <AdminLayout >
-    
+    <UserContext.Provider value={{ userData, setView: setCurrentView }}>
+      <AdminLayout>
         <h1>{currentView}</h1>
-    </AdminLayout>
+      </AdminLayout>
     </UserContext.Provider>
-    
-  )
-}
+  );
+};
 
-export const getServerSideProps = protectedAdminRoute((context,data) => {
-
+export const getServerSideProps = protectedAdminRoute((context, data) => {
   return {
     props: {
-      userData:data,
-    }
-  }
-}
-);
+      userData: data,
+    },
+  };
+});
 
 export default MainPage;
-
-
