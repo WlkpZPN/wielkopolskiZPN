212f8dbe962b5adcf184cfba978e31b6c322cedc
Aleksander <hondakkia@gmail.com>
2021-02-13 21:45:13 +0000

    improved protected admin route

--- middleware/protectedAdmin.tsx
+++ middleware/protectedAdmin.tsx
@@ -4,17 +4,19 @@ export function protectedAdminRoute(cb) {
   return async (context) => {
     const { req, res } = context;
     // let cookies;
+    let decodedToken;
     const token = req.cookies.userToken || null;
     if (!token) {
       res.statusCode = 302;
       res.setHeader("Location", "/admin/login");
-    }
-    const decodedToken = jwt.verify(token, process.env.AUTH_KEY);
-    console.log("token", decodedToken);
+    } else {
+      decodedToken = jwt.verify(token, process.env.AUTH_KEY);
+      console.log("token", decodedToken);
 
-    if (decodedToken.role !== 0) {
-      res.statusCode = 302;
-      res.setHeader("Location", "/admin/login");
+      if (decodedToken.role !== 0) {
+        res.statusCode = 302;
+        res.setHeader("Location", "/admin/login");
+      }
     }
 
     return await cb(context, decodedToken);
