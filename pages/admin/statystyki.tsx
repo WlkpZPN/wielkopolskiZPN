d1cd5385f1e4a69dc3b6398fe59127a034e4d0e3
WlkpZPN <licklub@wielkopolskizpn.pl>
2021-03-15 15:58:52 +0000

    usunieto jsx z ustawien i statystyk

--- pages/admin/statystyki.tsx
+++ pages/admin/statystyki.tsx
@@ -2,9 +2,12 @@ import AdminLayout from "../../components/organisms/admin_layout";
 
 const Statystyki = () => {
   return (
-    <AdminLayout view="statystyki">
-      <h1>Statystyki</h1>
-    </AdminLayout>
+    // <AdminLayout view="statystyki">
+    //   <h1>Statystyki</h1>
+    // </AdminLayout>
+    <div>
+      <p>Statystyki</p>
+    </div>
   );
 };
 
--- pages/admin/ustawienia.tsx
+++ pages/admin/ustawienia.tsx
@@ -2,9 +2,12 @@ import AdminLayout from "../../components/organisms/admin_layout";
 
 const Ustawienia = () => {
   return (
-    <AdminLayout view="ustawienia">
-      <h1>Ustawienia</h1>
-    </AdminLayout>
+    // <AdminLayout view="ustawienia">
+    //   <h1>Ustawienia</h1>
+    // </AdminLayout>
+    <div>
+      <p>ustawienia</p>
+    </div>
   );
 };
 
