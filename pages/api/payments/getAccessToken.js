e5078b301b514dc880a343d779c8641e6f37de5d
WlkpZPN <licklub@wielkopolskizpn.pl>
2021-03-20 19:43:33 +0000

    added payments api routes

---
+++ pages/api/payments/getAccessToken.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export default async (req, res) => {
+  return new Promise(async (resolve) => {
+    try {
+      const result = await axios({
+        method: "POST",
+        url: "https://secure.snd.payu.com/pl/standard/user/oauth/authorize",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        data:
+          "grant_type=client_credentials&client_id=404238&client_secret=c1b15e22e4031f006c1e1f97dafe665c",
+      });
+
+      res.send(result.data.access_token);
+    } catch (err) {
+      console.log(err);
+      res.send(err);
+    }
+  });
+};
