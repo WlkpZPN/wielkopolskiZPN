e7b2912751dcbe9180494735e1af8dffc9586514
WlkpZPN <licklub@wielkopolskizpn.pl>
2021-04-04 18:26:57 +0100

    added header file

---
+++ components/atoms/header.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Header = styled.h1`
+  padding: 40px 0;
+  color: ${({ theme, color }) => (color ? theme[color] : theme.dark)};
+  font-weight: 600;
+  font-size: 32px;
+`;
+
+export default Header;
