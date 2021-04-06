92d6e146a953c1f08bc10958113e1a97e67d4a12
WlkpZPN <licklub@wielkopolskizpn.pl>
2021-04-06 17:26:26 +0100

    test commit

--- pages/admin/statystyki.tsx
+++ pages/admin/statystyki.tsx
@@ -34,7 +34,7 @@ const Statystyki = ({ userData }) => {
       <PrimaryButton onClick={() => generatePdf()}>
         Pobierz licencje
       </PrimaryButton>
-      <p>this is develop branch</p>
+      <p>to jest testowa strona</p>
       {/* <Header>Wnioski robocze</Header>
       <ProgressContainer
         status={1}
