a44457ad81bf41ddca8000830718224778a5f497
WlkpZPN <licklub@wielkopolskizpn.pl>
2021-05-11 11:43:57 +0100

    disabled mailtrap

--- middleware/transporter.tsx
+++ middleware/transporter.tsx
@@ -1,21 +1,21 @@
 import nodemailer from "nodemailer";
 
-// const transporter = nodemailer.createTransport({
-//   host: "serwer1710802.home.pl",
-//   port: 465,
-//   auth: {
-//     user: "licklub@wielkopolskizpn.pl",
-//     pass: "licencje2020",
-//   },
-// });
-
 const transporter = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
+  host: "serwer1710802.home.pl",
+  port: 465,
   auth: {
-    user: "d6b6955cf480dd",
-    pass: "fb302668d87e65",
+    user: "licklub@wielkopolskizpn.pl",
+    pass: "licencje2020",
   },
 });
+
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.mailtrap.io",
+//   port: 2525,
+//   auth: {
+//     user: "d6b6955cf480dd",
+//     pass: "fb302668d87e65",
+//   },
+// });
 
 export default transporter;
