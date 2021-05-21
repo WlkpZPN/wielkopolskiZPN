ecc23f5ce226b0a875cb62e932c3d741a4607801
WlkpZPN <licklub@wielkopolskizpn.pl>
2021-05-21 17:41:33 +0100

    fixed password change for clubs

--- pages/api/clubAuth/updatePassword.js
+++ pages/api/clubAuth/updatePassword.js
@@ -12,32 +12,30 @@ export default (req, res) => {
           id: parseInt(clubID),
         },
       });
-      ``; //2. check if previous password is valid
-      bcrypt.compare(oldPassword, club.password, async (err, result) => {
-        if (result) {
-          // pasword is ok, update the club password
-          bcrypt.hash(newPassword, saltRounds, async (err, hash) => {
-            await prisma.clubs.update({
-              where: {
-                id: parseInt(clubID),
-              },
-              data: {
-                password: hash,
-              },
-            });
-          });
-        } else {
-          // password is not ok, return message
+      //2. check if previous password is valid
 
-          res.status(401);
-          res.json({
-            status: "Odmowa dostępu",
-            message:
-              "aktualne hasło nieprawidłowe,upewnij się że podajesz dobre hasło oraz zwróć uwagę na wielkość liter",
-          });
-          return resolve();
-        }
-      });
+      if (club.password === oldPassword) {
+        // pasword is ok, update the club password
+
+        await prisma.clubs.update({
+          where: {
+            id: parseInt(clubID),
+          },
+          data: {
+            password: newPassword,
+          },
+        });
+      } else {
+        // password is not ok, return message
+
+        res.status(401);
+        res.json({
+          status: "Odmowa dostępu",
+          message:
+            "aktualne hasło nieprawidłowe,upewnij się że podajesz dobre hasło oraz zwróć uwagę na wielkość liter",
+        });
+        return resolve();
+      }
     } catch (err) {
       console.log(err);
       res.status(400);
