4e1d6f5ddad8d761cd5b7969ea7fd4400d24258a
WlkpZPN <licklub@wielkopolskizpn.pl>
2021-06-07 13:31:33 +0100

    pdf fixes

--- middleware/generatePdf.js
+++ middleware/generatePdf.js
@@ -366,7 +366,7 @@ export const generatePdf = async (clubData, date = null, dwn = true) => {
 
     textField2.addToPage(page, {
       x: 50,
-      y: 200,
+      y: 170,
       font: regular,
       textColor: rgb(0, 0, 0),
       borderColor: rgb(1, 1, 1),
