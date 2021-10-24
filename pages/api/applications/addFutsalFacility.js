21ce16ea5c7f1df4fea80f5e1dd35f5d095d1560
WlkpZPN <licklub@wielkopolskizpn.pl>
2021-10-24 12:42:37 +0100

    updated platfrom with new functions and added futsal leauge

--- pages/api/applications/addFutsalFacility.js
+++ pages/api/applications/addFutsalFacility.js
@@ -3,33 +3,28 @@ import prisma from "../../../middleware/prisma";
 export default async (req, res) => {
   return new Promise(async (resolve) => {
     const { sport_facility, clubData } = req.body;
-    console.log(sport_facility, clubData);
+
     let new_object = {};
 
     if (sport_facility) {
       delete sport_facility.applications_attachments;
     }
 
-    if (sport_facility?.id) {
-      new_object = await prisma.futsal_facilities.update({
-        where: {
-          id: sport_facility.id,
-        },
-        data: {
-          application_id: clubData.applications[0].id,
-          ...sport_facility,
-        },
-      });
+    new_object = await prisma.futsal_facilities.upsert({
+      where: {
+        id: sport_facility.id,
+      },
+      update: {
+        application_id: clubData.applications[0].id,
+        ...sport_facility,
+      },
+      create: {
+        application_id: clubData.applications[0].id,
+        ...sport_facility,
+      },
+    });
 
-      console.log("futsal facility", sport_facility);
-    } else if (!sport_facility?.id) {
-      new_object = await prisma.futsal_facilities.create({
-        data: {
-          application_id: clubData.applications[0].id,
-          ...sport_facility,
-        },
-      });
-    }
+    console.log("futsal facility", sport_facility);
 
     await prisma.$disconnect();
 
--- pages/api/mails/applicationSendedMail.js
+++ pages/api/mails/applicationSendedMail.js
@@ -8,7 +8,7 @@ export default (req, res) => {
       transporter.sendMail({
         from: "licklub@wielkopolskizpn.pl",
         to: email,
-        subject: "WielkopolskiZPN - popraw swój wniosek licencyjny",
+        subject: "WielkopolskiZPN - złożno wniosek licencyjny",
         html: `<head>
   <link rel="preconnect" href="https://fonts.gstatic.com" />
   <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet" />
@@ -30,18 +30,18 @@ export default (req, res) => {
       </tr>
       <tr style="padding: 0">
         <td style="margin: 0; padding: 16px 48px; background-color: #0156a6">
-          <h1 style="font-weight:bold; margin: 0; padding: 0; color: white">Twój wniosek został skierowany do poprawy</h1>
+          <h1 style="font-weight:bold; margin: 0; padding: 0; color: white">Wniosek licencyjny pomyślnie złożony</h1>
         </td>
       </tr>
       <tr>
         <td style="padding: 32px 48px">
         <p style='max-width:400px; color:rgba(0,0,0,0.8); font-weight:bold;'>
 
-        Twój wniosek został skierowany do poprawy z powodu:
+      
         ${description}
         
            <p style='max-width:400px; color:rgba(0,0,0,0.8);' >
-          Zaloguj się na swoje konto na platformie licencyjnej, aby wprowadzić poprawki.  Następnie wyślij wniosek ponownie.
+          Zaloguj się na swoje konto na platformie licencyjnej, aby sprawdzić przebieg wydania licencji.
         </p>
         </>
          
